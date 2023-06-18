c2f12e3ba3be0f7fd1414de0f728f17a730a4003
Santiago Gonzalez <santtiagogp@MacBook-Pro-de-Santiago.local>
2023-06-18 15:26:38 -0500

    import fixed

--- lib/ui/utils/app_routes.dart
+++ lib/ui/utils/app_routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/ui/screens/home/home_screen.dart';
+import '../screens/home/home_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   HomeScreen.screenName : ( context ) => const HomeScreen()
