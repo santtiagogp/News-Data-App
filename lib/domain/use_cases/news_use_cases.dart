e2d2e710002803825274d9e05ad8da4911d4cd32
Santiago Gonzalez <santtiagogp@MacBook-Pro-de-Santiago.local>
2023-06-25 18:32:15 -0500

    usecases added

---
+++ lib/domain/use_cases/news_use_cases.dart
@@ -0,0 +1,13 @@
+import '../entities/latest_news_model.dart';
+
+import '../repositories/news_repository.dart';
+
+class NewsUseCases {
+
+  NewsUseCases( this._newsRepository );
+
+  final NewsRepository _newsRepository;
+
+  Future<LastestNews> getLatestNews() => _newsRepository.getLastestNews();
+
+}
