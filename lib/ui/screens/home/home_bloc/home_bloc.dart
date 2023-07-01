bd2b1812986d514cee7e7205083efc372993898b
Santiago Gonzalez <santtiagogp@MacBook-Pro-de-Santiago.local>
2023-07-01 15:58:34 -0500

    removed unused import

--- lib/ui/screens/home/home_bloc/home_bloc.dart
+++ lib/ui/screens/home/home_bloc/home_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
-import '../../../../domain/entities/latest_news_model.dart';
 import '../../../../domain/entities/news_model.dart';
 import '../../../../domain/use_cases/news_use_cases.dart';
 
