87df0b036473d41f796d9e34134c53e7fde2123e
Santiago <santiago.gp004@gmail.com>
2023-07-04 21:28:45 -0500

    News page added

--- lib/ui/screens/news/news_page.dart
+++ lib/ui/screens/news/news_page.dart
@@ -10,12 +10,46 @@ class NewsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-  final args = ModalRoute.of(context)!.settings.arguments as News;
+    final args = ModalRoute.of(context)!.settings.arguments as News;
+
+    final primaryColor = Theme.of(context).primaryColor;
 
     return Scaffold(
-      body: Center(
-        child: Text(args.title),
+
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: Text(args.title),
       ),
+      body: ListView(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text(
+                  args.title,
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500
+                  )
+                ),
+                const SizedBox( height: 20 ),
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Image(
+                    image: NetworkImage(args.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox( height: 20 ),
+                Text(args.content, style: const TextStyle(fontSize: 20)),
+              ],
+            )
+          )
+        ],
+      )
+
     );
   }
 }
