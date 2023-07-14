e3200fee14d6dd5b04aea7a59a99dc878dec4223
Santiago <santiago.gp004@gmail.com>
2023-07-13 22:15:22 -0500

    Saved screen showing news tile

--- lib/ui/screens/saved/saved_screen.dart
+++ lib/ui/screens/saved/saved_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/ui/screens/home/widgets/news_tile.dart';
 import 'package:news_app/ui/screens/saved/saved_bloc/saved_bloc.dart';
 
 class SavedScreen extends StatelessWidget {
@@ -14,7 +15,28 @@ class SavedScreen extends StatelessWidget {
       ),
       body: BlocBuilder<SavedBloc, SavedState>(
         builder: (context, state) {
-          return Container();
+
+          if( state is SavedNews ) {
+            return ListView.builder(
+              itemCount: state.news.length,
+              itemBuilder: (context, index) {
+
+                final news = state.news[index];
+
+                return NewsTile(
+                  title: news.title,
+                  description: news.description,
+                  imgUrl: news.imageUrl,
+                  date: news.pubDate,
+                  onTap: (){},
+                  onIconTap: (){}
+                );
+              },
+            );
+          }
+
+          return const Center(child: CircularProgressIndicator());
+
         },
       ),
     );
